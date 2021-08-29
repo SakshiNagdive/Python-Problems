849715bcb29e8b82c8d700582e2adf1cb8afd865
SakshiNagdive <89695784+SakshiNagdive@users.noreply.github.com>
2021-08-29 08:36:11 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,9 @@
+#WAP to Calculate Simple Intrest
+
+P = int(input("Enter the principle amount:"))
+T = int(input("Enter the time:"))
+R = int(input("Enter the rate"))
+
+Simple_Interest = P * R * T / 100
+
+print("The Simple Interest is:" ,Simple_Interest)
---
+++ problem8.py
@@ -0,0 +1,6 @@
+#WAP to rever the three digit number
+
+Num = int(input("Enter a three digit number:"))
+
+Num.index(1)
+print(Num)
