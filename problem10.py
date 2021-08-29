0fc1584e7433f798b7f3d264210823c885f8d93a
Sakshi Nagdive <89695784+SakshiNagdive@users.noreply.github.com>
2021-08-29 22:31:35 +0530

    Delete problem10.py

--- problem10.py
+++
@@ -1,7 +0,0 @@
-#WAP to find Circumference of circle
-
-Radius=int(input("Enter the radius of circle:"))
-
-Circumference_of_circle = 2 * 3.14 * Radius
-
-print("Area of circle is :" ,Circumference_of_circle)
