8f9cc29fa0ae12bf5e3f06520efc0ee9767b4556
Sakshi Nagdive <89695784+SakshiNagdive@users.noreply.github.com>
2021-08-31 23:26:02 +0530

    Add files via upload

---
+++ SDP-DAY-1/problem9.py
@@ -0,0 +1,5 @@
+#WAP to convert centigrate teprature and calculate the temperature in Fahrenheit  
+
+centigrate = float(input('Enter temperature in centigrate: '))  
+fahrenheit = ( 1.8 * centigrate) + 240
+print("Temprature in fahrenheit:",fahrenheit) 
